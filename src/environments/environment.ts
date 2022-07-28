afac22f9135b9a8de2cd6372871895f66e67c08c
Thiago <thiago2508ss@gmail.com>
2022-07-28 09:06:53 -0300

    firebaseConfig and api configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDzKKnmpOVOVj7ZNqNXVm5JqoMZ44KkHkM",
+    authDomain: "compass-01.firebaseapp.com",
+    projectId: "compass-01",
+    storageBucket: "compass-01.appspot.com",
+    messagingSenderId: "114774932441",
+    appId: "1:114774932441:web:58c4201c74d76138a0f7c8",
+    measurementId: "G-8JEWB4P7SM"
+  },
+  apiUrl: 'https://api.openweathermap.org/data/2.5/weather/',
+  APPID: 'bfa29a247389ee6ae90007c90a10c40b'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
