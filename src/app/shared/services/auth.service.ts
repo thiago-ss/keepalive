867199d5850ec18e0aa44ade31ee38dd8887d92d
Thiago <thiago2508ss@gmail.com>
2022-07-28 09:07:09 -0300

    authentication service

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private fireAuth: AngularFireAuth, private router: Router) {}
+
+  login(email: string, password: string) {
+    this.fireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      localStorage.setItem('token', 'true');
+      this.router.navigate(['/home'])
+    }, err => {
+      alert(err.message);
+      this.router.navigate(['/login'])
+    });
+  }
+
+  register(email: string, password: string) {
+    this.fireAuth.createUserWithEmailAndPassword(email, password).then(() => {
+      alert('Registration successful');
+      this.router.navigate(['/login']);
+    }, err => {
+      alert(err.message);
+      this.router.navigate(['/register']);
+    });
+  }
+
+  logout() {
+    this.fireAuth.signOut().then(() => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login'])
+    }, err => {
+      alert(err.message);
+    });
+  }
+}
