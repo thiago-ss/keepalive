1e1ab343c884088dcb92c20482ebd10694b1c388
Thiago <thiago2508ss@gmail.com>
2022-07-28 09:07:46 -0300

    weather service

---
+++ src/app/shared/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private httpClient: HttpClient) {}
+
+  getWeatherData(lat: number, lon: number) {
+    let params = new HttpParams()
+    .set('lat', lat)
+    .set('lon', lon)
+    .set('units', 'metric')
+    .set('appid', environment.APPID)
+
+    return this.httpClient.get(environment.apiUrl, { params });
+  }
+}
