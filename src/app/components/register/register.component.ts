8c3409d125ccf0fa4231462b6c6935e38919edb0
Thiago <thiago2508ss@gmail.com>
2022-08-02 12:09:13 -0300

    responsive register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,31 +1,136 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  animations: [
+    trigger('warning', [
+      state('normal', style({
+        borderColor: '#FFF'
+      })),
+      state('yellow', style({
+        borderColor: '#E9B425'
+      })),
+      transition('normal <=> yellow', [
+        animate('0.5s')
+      ])
+    ])
+  ]
 })
 export class RegisterComponent implements OnInit {
 
   email: string = '';
   password: string = '';
+  confirmPassword: string = '';
 
-  constructor(private authService: AuthService) {}
+  isError: boolean = false;
+  isEmailError: boolean = false;
+  isPasswordError: boolean = false;
+  isConfirmPasswordError: boolean = false;
+
+  validEmail: any;
+  validPassword: any;
+
+  registerForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{6,15}$")
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{6,15}$")
+    ])
+  })
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
-  register() {
-    if(this.email == '' || this.password == '') {
-      alert('Ops, usuario ou senha invalidos. Tente novamente!');
-      return;
+  errorHandler() {
+    if(this.registerForm.controls['email'].errors) {
+      this.isEmailError = true;
+    } else {
+      this.isEmailError = false;
     }
 
-    this.authService.register(this.email, this.password);
+    if(this.registerForm.controls['password'].errors) {
+      this.isPasswordError = true;
+    } else {
+      this.isPasswordError = false;
+    }
+
+    if(this.registerForm.controls['confirmPassword'].errors) {
+      this.isConfirmPasswordError = true;
+    } else {
+      this.isConfirmPasswordError = false;
+    }
+  }
+
+  validate() {
+    this.validEmail = this.registerForm.controls['email'].errors?.['pattern'] && this.registerForm.controls['email'].errors?.['required'];
+    this.validPassword = this.registerForm.controls['password'].errors?.['pattern'] && 
+      this.registerForm.controls['password'].errors?.['required'] &&
+      this.registerForm.controls['confirmPassword'] == this.registerForm.controls['password']
     
-    this.email = '';
-    this.password = '';
+    if(this.validEmail == true && this.validPassword == true) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  haveLowercase(param: string) {
+    let regEx = /[a-z]/;
+    return regEx.test(param);
+  }
+
+  haveUppercase(param: string) {
+    let regEx = /[A-Z]/;
+    return regEx.test(param);
+  }
+
+  haveNumber(param: string) {
+    let regEx = /[0-9]/;
+    return regEx.test(param);
+  }
+
+  haveSymbols(param: string) {
+    let regEx = /[$&+,:;=?@#|'<>.^*()%!-]/;
+    return regEx.test(param);
+  }
+
+  register() {
+    if(!this.registerForm.valid && !this.registerForm.dirty) {
+      this.isError = true;
+    } else {
+      this.authService.register(this.email, this.password);
+    
+      this.email = '';
+      this.password = '';
+    }
+  }
+
+  get Email() {
+    return this.registerForm.get('email')
+  }
+
+  get Password() {
+    return this.registerForm.get('password')
+  }
+
+  get ConfirmPassword() {
+    return this.registerForm.get('confirmPassword')
   }
 
 }
