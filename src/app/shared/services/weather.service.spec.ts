a607f127a2e4b217a3f81747383c59a509b52b58
Thiago <thiago2508ss@gmail.com>
2022-08-10 14:31:16 -0300

    link style change

--- src/app/shared/services/weather.service.spec.ts
+++ src/app/shared/services/weather.service.spec.ts
@@ -1,16 +1,41 @@
-import { TestBed } from '@angular/core/testing';
-
+import { async, inject, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 import { WeatherService } from './weather.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 describe('WeatherService', () => {
   let service: WeatherService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [WeatherService]
+    });
     service = TestBed.inject(WeatherService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  let lat: number = 0;
+  let lon: number = 0;
+
+  let params = new HttpParams()
+    .set('lat', lat)
+    .set('lon', lon)
+    .set('units', 'metric')
+    .set('appid', environment.APPID)
+
+  it(`should issue a request`,
+    async(
+      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+        http.get(environment.apiUrl, { params }).subscribe();
+        backend.expectOne({
+          url: 'r',
+          method: 'GET'
+        });
+      })
+    )
+  );
 });
