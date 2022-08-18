fde13311c707c92a653964eb6b7e963f418c7e2e
Thiago <thiago2508ss@gmail.com>
2022-08-18 10:56:44 -0300

    inferring return type

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'any'
@@ -9,7 +10,7 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getWeatherData(lat: number, lon: number) {
+  getWeatherData(lat: number, lon: number): Observable<any> {
     let params = new HttpParams()
     .set('lat', lat)
     .set('lon', lon)
